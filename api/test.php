0f2eb4b9ce9776789264184c2530b5064c74e339
Kokimoto <suportekokimoto@gmail.com>
2026-07-26 15:44:31 -0300

    Clean vercel.json - remove installCommand, test.php, keep buildCommand only

--- api/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'php_version' => phpversion(),
-    'extensions' => get_loaded_extensions(),
-    'memory_limit' => ini_get('memory_limit'),
-    'max_execution_time' => ini_get('max_execution_time'),
-]);
